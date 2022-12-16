f5dd60f1a5627e5e257970be220cdb178b311f0c
Rakesh-Choudhary175 <rakeshchoudhary.rc917@gmail.com>
2022-12-17 02:01:10 +0530

    Solved Question Description Added
    Signed-off-by: Rakesh-Choudhary175 <rakeshchoudhary.rc917@gmail.com>

--- src/components/questions/SolveQuestion.js
+++ src/components/questions/SolveQuestion.js
@@ -1,44 +1,55 @@
 import React from "react";
 import Header from "../Header";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, DropdownButton, Dropdown, Nav } from "react-bootstrap";
 function SolveQuestion() {
     return (
-        // <div>
-        //     <h1>God</h1>
-        // </div>
-        <div>
+
+        <>
+
             <Header />
-            <Button variant="primary">Description</Button>{
-                <Card style={{ position: "fixed", left: "5%", width: '35rem' }}>
-                    <Card.Body>
-                        <Card.Title>Question Title</Card.Title>
-                        <Card.Text>
-                            Question Description
-                        </Card.Text>
-                        <Card.Title>Example</Card.Title>
-                        <Card.Text>
-                            Question Example
-                        </Card.Text>
-                        <Card.Title>Input Format</Card.Title>
-                        <Card.Text>
-                            Question Input Format
-                        </Card.Text>
-                        <Card.Title>Output Format</Card.Title>
-                        <Card.Text>
-                            Question Output Format
-                        </Card.Text>
-                        <Card.Title>Sample Input</Card.Title>
-                        <Card.Text>
-                            Question Sample Input
-                        </Card.Text>
-                        <Card.Title>Sample Output</Card.Title>
-                        <Card.Text>
-                            Question Sample Output
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            }
-        </div>
+
+
+            <Card className="questionData" style={{ width: '35rem' }}>
+                <Card.Header>
+                    <Nav justify variant="tabs" defaultActiveKey="/home">
+                        <Nav.Item>
+                            <Nav.Link eventKey="questionData">Description</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="Comment">Comment</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Title>Question Title</Card.Title>
+                    <Card.Text>
+                        Question Description
+                    </Card.Text>
+                    <Card.Title>Example</Card.Title>
+                    <Card.Text>
+                        Question Example
+                    </Card.Text>
+                    <Card.Title>Input Format</Card.Title>
+                    <Card.Text>
+                        Question Input Format
+                    </Card.Text>
+                    <Card.Title>Output Format</Card.Title>
+                    <Card.Text>
+                        Question Output Format
+                    </Card.Text>
+                    <Card.Title>Sample Input</Card.Title>
+                    <Card.Text>
+                        Question Sample Input
+                    </Card.Text>
+                    <Card.Title>Sample Output</Card.Title>
+                    <Card.Text>
+                        Question Sample Output
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+        </>
+
 
     );
 }
