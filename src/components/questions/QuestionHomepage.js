6961debf73b236c81b21f370f65f3f6ab3c01bed
Rakesh-Choudhary175 <rakeshchoudhary.rc917@gmail.com>
2022-12-16 19:17:22 +0530

    Question Css added
    Signed-off-by: Rakesh-Choudhary175 <rakeshchoudhary.rc917@gmail.com>

--- src/components/questions/QuestionHomepage.js
+++ src/components/questions/QuestionHomepage.js
@@ -1,54 +1,53 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Card ,Button} from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import Header from "../Header";
 import url from "../../Uri";
 import "./QuestionHomepage.css"
 
-function QuestionHomepage(){
+function QuestionHomepage() {
 
-    const [questions,SetQuestions] = useState([]);
-    const [areQuestionsFetched,SetAreQuestionsFetched]= useState(false);
+    const [questions, SetQuestions] = useState([]);
+    const [areQuestionsFetched, SetAreQuestionsFetched] = useState(false);
 
-    useEffect(()=>{
-        if(!areQuestionsFetched){
-            axios.get(url+"/api/v1/question").then(function(response){
-                console.log("success:",response.data);
-                response.data.data.question.map((question)=>{
+    useEffect(() => {
+        if (!areQuestionsFetched) {
+            axios.get(url + "/api/v1/question").then(function (response) {
+                console.log("success:", response.data);
+                response.data.data.question.map((question) => {
                     questions.push(question);
                 })
                 SetAreQuestionsFetched(true);
-            }).catch(function(error){
+            }).catch(function (error) {
                 console.log(error);
             })
         }
     })
 
-    function fetchQuestionDetails(){
+    function fetchQuestionDetails() {
         alert("Question clicked")
     }
 
     return (
         <div>
-            <Header/>
+            <Header />
             {
-                questions.map((question)=>{
-                    return(
-                        <Card style={{width:"1000px",background:"#ABCDEF",marginLeft:"20px"}} onClick={fetchQuestionDetails}>
+                questions.map((question) => {
+                    return (
+                        <Card class="card" style={{ width: "1000px", marginLeft: "20px" }} onClick={fetchQuestionDetails}>
                             <Card.Body>
-                            <Card.Title>{question.title}</Card.Title>
-                            <Card.Text>
-                                Difficulty:{question.difficulty}&nbsp;&nbsp;|&nbsp; Score:{question.score}&nbsp;&nbsp;|&nbsp;Category:{question.category} 
-                            </Card.Text>
-                            <Card.Text className="overflow-wrap">
-                                {question.description}
-                            </Card.Text>
+                                <Card.Title>&nbsp;{question.title}</Card.Title>
+                                <Card.Text>
+                                    &nbsp;Difficulty:{question.difficulty}&nbsp;&nbsp;|&nbsp; Score:{question.score}&nbsp;&nbsp;|&nbsp;Category:{question.category}
+                                </Card.Text>
+                                <Card.Text className="overflow-wrap">
+                                    &nbsp;{question.description}&nbsp;
+                                </Card.Text>
                             </Card.Body>
                         </Card>
                     );
                 })
             }
-            
         </div>
     );
 }
