b27aa5038beb21bef167577cb5e849bfea390572
Shivam Verma <shivam.r.verma@seagate.com>
2022-12-16 19:10:51 +0530

    Added header bootstrap
    Signed-off-by: Shivam Verma <shivam.r.verma@seagate.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,39 @@
 import React from "react";
 import logo from "../assets/logo.png"
 import {Button, Col, Row} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate } from "react-router-dom";
+
 function Header(){
+
+    const navigate = useNavigate();
+
+    function getQuestions(){
+        navigate('/questions')
+    }
+
     return (
         <div>
         <Row>
             <Col md={1}>
-                <img src={logo} style={{height:"100px",width:"100px"}}/>
-                
+                <img src={logo} style={{height:"50px",width:"50px"}}/>    
             </Col>
             <Col md={1}>
-            <h5>Gyaandock</h5>
+                <h5>Gyaandock</h5>
             </Col>
+            <Col md={6}>
+            </Col>
+            <Col md={1}>
+                <Button onClick={getQuestions}>Questions</Button>
+            </Col>
+            <Col md={1}>
+                <Button>Challenges</Button>
+            </Col>
+            <Col md={1}>
+                <Button>Sign In</Button>
+            </Col>    
         </Row>
+        <br/>
         
         </div>
     );
