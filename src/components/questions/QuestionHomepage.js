a75f880edef32fdb51d03599f18528443104b9ca
Rakesh-Choudhary175 <rakeshchoudhary.rc917@gmail.com>
2022-12-17 00:12:55 +0530

    Dropdown list in question added
    Signed-off-by: Rakesh-Choudhary175 <rakeshchoudhary.rc917@gmail.com>

--- src/components/questions/QuestionHomepage.js
+++ src/components/questions/QuestionHomepage.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Dropdown, Table } from "react-bootstrap";
 import Header from "../Header";
 import url from "../../Uri";
 import "./QuestionHomepage.css"
@@ -29,18 +29,61 @@ function QuestionHomepage() {
 
     function fetchQuestionDetails(question) {
         // alert("Question clicked:"+question.id)
-        navigate('/questions/'+question.id)
+        navigate('/questions/' + question.id)
 
     }
 
     return (
         <div>
             <Header />
+            <>
+                <Dropdown className="d-inline mx-4">
+                    <Dropdown.Toggle >
+                        Difficulty Level
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#">Easy</Dropdown.Item>
+                        <Dropdown.Item href="#">Medium</Dropdown.Item>
+                        <Dropdown.Item href="#">Hard</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+
+                <Dropdown className="d-inline mx-5">
+                    <Dropdown.Toggle >
+                        Status
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#">Solved</Dropdown.Item>
+                        <Dropdown.Item href="#">Unsolved</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+
+                <Dropdown className="d-inline mx-5">
+                    <Dropdown.Toggle>
+                        Category
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#">String</Dropdown.Item>
+                        <Dropdown.Item href="#">Classes</Dropdown.Item>
+                        <Dropdown.Item href="#">STL</Dropdown.Item>
+                        <Dropdown.Item href="#">Inheritance</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+            </>
             {
                 questions.map((question) => {
                     return (
                         <div>
-                            <Card className="question" style={{ width: "1200px", marginLeft: "20px" }} onClick={()=>fetchQuestionDetails(question)}>
+                            <Card className="question" style={{ width: "1200px", marginLeft: "20px" }} onClick={() => fetchQuestionDetails(question)}>
                                 <Card.Body>
                                     <Card.Title>{question.title}</Card.Title>
                                     <Card.Text>
