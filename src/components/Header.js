711bb00277b8ad39db60e378a0ef3e266b954dec
Shivam Verma <shivam.r.verma@seagate.com>
2022-12-16 19:16:02 +0530

    working on header
    Signed-off-by: Shivam Verma <shivam.r.verma@seagate.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,20 +16,21 @@ function Header(){
         <div>
         <Row>
             <Col md={1}>
-                <img src={logo} style={{height:"50px",width:"50px"}}/>    
+                <img src={logo} style={{textAlign:"right",height:"50px",width:"50px",margin:"5px"}}/> 
+                  
             </Col>
             <Col md={1}>
-                <h5>Gyaandock</h5>
+                <h5 style={{margin:"20px",textAlign:"left"}}>Gyaandock</h5> 
             </Col>
             <Col md={6}>
             </Col>
             <Col md={1}>
-                <Button onClick={getQuestions}>Questions</Button>
+                <Button style={{marginTop:"20px"}} onClick={getQuestions}>Questions</Button>
             </Col>
-            <Col md={1}>
+            <Col md={1} style={{marginTop:"20px"}}>
                 <Button>Challenges</Button>
             </Col>
-            <Col md={1}>
+            <Col md={1}style={{marginTop:"20px"}}>
                 <Button>Sign In</Button>
             </Col>    
         </Row>
