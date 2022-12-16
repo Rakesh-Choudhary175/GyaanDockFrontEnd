cb45acd25726c577d000a0f82c45273039ea372e
Rakesh-Choudhary175 <rakeshchoudhary.rc917@gmail.com>
2022-12-16 23:31:41 +0530

    About page modified
    Signed-off-by: Rakesh-Choudhary175 <rakeshchoudhary.rc917@gmail.com>

--- src/components/aboutUs/aboutUs.js
+++ src/components/aboutUs/aboutUs.js
@@ -1,5 +1,5 @@
 import Header from "../Header";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Table } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function aboutUspage() {
@@ -8,7 +8,7 @@ function aboutUspage() {
         <div>
             <Header />
             <Card className="text-center" style={{
-                width: '1000px', background: "rgba(0, 152, 186, 0.4)", position: "fixed", top: "50%",
+                width: '1000px', background: "rgba(0, 152, 186, 0.4)", position: "fixed", top: "35%",
                 left: "50%", transform: "translate(-50%, -50%)"
             }}>
                 <Card.Body>
@@ -22,120 +22,61 @@ function aboutUspage() {
                 </Card.Body>
             </Card>
 
-            <div id="team">
-                <div >
-                    <p>
-                        <span>Developer Team</span>
-                    </p>
-                    <p >
-                        <span>Tanishqa Raut</span>
-                    </p>
-                    <p >
-                        <span>Vishwajeet Singh</span>
-                    </p>
-                    <p >
-                        <span>Shivanjali Thopte</span>
-                    </p>
-                    <p >
-                        <span>Swejal Gabhane</span>
-                    </p>
-                </div>
-                <div >
-                    <span >
-                        <p >
-                            <span>CONTACT US</span>
-                        </p>
-                        <p >
-                            <span >
-                                Email Id: xyz@gmail.com
-                            </span>
-                        </p>
-                        <p >
-                            <span>
-                                Phone.No. : 1234567890
-                            </span>
-                        </p>
-                        <p >
-                            <span >&nbsp;</span>
-                        </p>
-                        <p >
-                            <span>{`GyaanDock©  `}</span>
-                        </p>
-                    </span>
-                </div>
-            </div >
-        </div >
-
+            <Card style={{
+                width: '1000px', position: "fixed", top: "55%",
+                left: "50%", transform: "translate(-50%)"
+            }}>
+                <Card.Body>
+                    <Table borderless responsive>
+                        <thead>
+                            <tr>
+                                <th>Developer Team</th>
+                                <th><p>&nbsp;</p></th>
+                                <th><p>&nbsp;</p></th>
+                                <th><p>&nbsp;</p></th>
+                                <th><p>&nbsp;</p></th>
+                                <th>CONTACT US</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Tanishqa Raut</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>Email Id: xyz@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>Vishwajeet Singh</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>Phone.No. : 1234567890</td>
+                            </tr>
+                            <tr>
+                                <td>Shivanjali Thopte</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td>Swejal Gabhane</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td>{`GyaanDock©  `}</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Card.Body>
+            </Card>
+        </div>
     );
 }
 
 export default aboutUspage;
-
-
-
-
-
-
-{/* 
-import styles from "./AboutUs.module.css";
-
-const AboutUs = () => {
-  return (
-    <div className={styles.aboutUs}>
-      <div className={styles.groupDiv}>
-        <img
-          className={styles.rectangleIcon}
-          alt=""
-          src="../rectangle-11.svg"
-        />
-        <div className={styles.ourAimIsToProvideAnEasy}>
-          <p
-            className={styles.ourAimIs}
-          >{`Our aim is to provide an easy-to-use coding platform where you will be able to see different programming questions from every domain to solve those challenges. `}</p>
-          <p className={styles.ourAimIs}>&nbsp;</p>
-          <p className={styles.ourAimIs}>
-            Our website will provide an open platform for all the amateurs as
-            well the tech geeks to practice and improve their coding skills in a
-            challenging way.
-          </p>
-          <p className={styles.swejalGabhane}>&nbsp;</p>
-        </div>
-      </div>
-      <div className={styles.header}>
-        <img
-          className={styles.appleTouchIcon2}
-          alt=""
-          src="../appletouchicon-2@2x.png"
-        />
-        <div className={styles.leftNest}>
-          <div className={styles.headerLinkBlock}>
-            <div className={styles.linkItem}>
-              <div className={styles.gyaanDock}>{`GyaanDock `}</div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.rightNest} />
-        <div className={styles.headerCTABlock}>
-          <Button variant="primary">Challenges</Button>
-          <Button variant="primary">About Us</Button>
-          <Button className={styles.buttonLoginsignup} variant="primary">
-            Login/ SignUp
-          </Button>
-        </div>
-      </div>
-      
-      </div>
-    </div>
-  );
-};
-
-export default AboutUs; */}
-
-
-
-
-
-
-
-
-
